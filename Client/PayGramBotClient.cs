1485ade3c1a04ce0821914192b960ead72767920
Luigi <Luigi@DSKTP-DXB-LUIGI>
2022-11-06 12:10:29 +0400

    minor

--- Client/PayGramBotClient.cs
+++ Client/PayGramBotClient.cs
@@ -28,7 +28,7 @@ namespace PayGram.Public.Client
 		/// <param name="curSym">The currency symbol, any of the <see cref="CurrenciesLib.Currencies"/> .ToString()</param>
 		/// <param name="unique">The unique code for this request</param>
 		/// <returns>True if it succeded, false otherwise</returns>
-		public async Task<bool> TransferMoneyAsync(long toTid, double amount, string curSym, string unique)
+		public async Task<bool> TransferMoneyAsync(long toTid, double amount, Currencies curSym, string unique)
 		{
 			if (amount <= 0) return false;
 			var response = await ExecuteMethodAsync(Token, PayGramHelper.TRANSFER_METHOD, $"{PayGramHelper.CURRENCY_SYMBOL_TOKEN_NAME}={curSym}&{PayGramHelper.AMT_TOKEN_NAME}={amount.ToString(CultureInfo.InvariantCulture)}&{PayGramHelper.TO_TOKEN_NAME}={toTid}&{PayGramHelper.UNIQUE_TOKEN_NAME}={unique}", null);
