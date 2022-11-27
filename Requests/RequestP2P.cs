9f3057f49cc5e83bfbe5b9a13a4a83fe2b0dd7e9
Pahan Sri Madusanka Rodrigo <pahanimg10@gmail.com>
2022-11-27 15:00:21 +0400

    P2P Group Response and Request Class add to the sourse

---
+++ Requests/RequestP2P.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayGram.Public.Requests
+{
+    public class RequestP2P : PaygramRequest
+    {
+    }
+}
