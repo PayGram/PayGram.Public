54c03261d6ffce99afd6f3689147825ce78e640b
Luigi Menghini <menghini@gmail.com>
2023-01-15 16:50:41 +0400

    red envelope

--- Requests/RequestCreateRedEnvelope.cs
+++ Requests/RequestCreateRedEnvelope.cs
@@ -1,8 +1,18 @@
-﻿namespace PayGram.Public
+﻿using CurrenciesLib;
+
+namespace PayGram.Public
 {
 	public class RequestCreateRedEnvelope : PaygramRequest
     {
-        public int MaxNumOfRedeemers { get; set; }
+		/// <summary>
+		/// The currency for this invoice
+		/// </summary>
+		public Currencies CurrencyCode { get; set; }
+		public int MaxNumOfRedeemers { get; set; }
         public decimal Amount { get; set; }
-    }
+		/// <summary>
+		/// The data to send back to the user when this invoice is paid
+		/// </summary>
+		public string? CallbackData { get; set; }
+	}
 }
