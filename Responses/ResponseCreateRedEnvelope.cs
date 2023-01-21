0569da72c222d4c1138d055e0bf1541ed322e1de
Luigi Menghini <menghini@gmail.com>
2023-01-21 19:02:00 +0400

    response fields in createenvelope

--- Responses/ResponseCreateRedEnvelope.cs
+++ Responses/ResponseCreateRedEnvelope.cs
@@ -2,6 +2,8 @@
 {
 	public class ResponseCreateRedEnvelope : PaygramResponse
 	{
+		public Guid InvoiceCode { get; set; }
+		public decimal Fees { get; set; }
 		public ResponseCreateRedEnvelope() : base(PaygramResponseTypes.ResponseCreateRedEnvelope)
 		{
 
