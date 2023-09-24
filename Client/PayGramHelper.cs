d0c91b72d18e33a50a6f67c431d225e6cc864d67
Luigi Menghini <menghini@gmail.com>
2023-09-24 19:09:00 +0400

    update to new paygram api

--- Client/PayGramHelper.cs
+++ Client/PayGramHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using CurrenciesLib;
+using System.Globalization;
 using Telegram.Messaging.Types;
 using Utilities.String.Extentions;
 
@@ -18,8 +19,8 @@ namespace PayGram.Public.Client
 		public const string SWAP_METHOD = "Swap";
 		public const string ISSUEINVOICE_METHOD = "IssueInvoice";
 
-        // tokens are used in the PayGramUsersController as query string parameters
-        public const string INVOICEID_TOKEN_NAME = "invguid";
+		// tokens are used in the PayGramUsersController as query string parameters
+		public const string INVOICEID_TOKEN_NAME = "invguid";
 		public const string TO_TOKEN_NAME = "to";
 		public const string CURRENCY_SYMBOL_TOKEN_NAME = "cursym";
 		public const string NETWORK_SYMBOL_TOKEN_NAME = "network";
@@ -39,17 +40,17 @@ namespace PayGram.Public.Client
 
 		public const string ACTION_MAKEVOUCHER = "v";
 		public const string ACTION_PAY = "p";
-        public const string ACTION_MAKEINVOICE = "i";
-        public const string ACTION_CANCELWITHDRAW = "w";
+		public const string ACTION_MAKEINVOICE = "i";
+		public const string ACTION_CANCELWITHDRAW = "w";
 
 
-        /// <summary>
-        /// Makes a link to the PayGram bot specifing the shown label and eventually the parameters that follow the start
-        /// </summary>
-        /// <param name="label">What the user will see</param>
-        /// <param name="query">The parameters that follow the start. Should not include start=. If it is not a base64 string, it will be converted to base64</param>
-        /// <returns>The text containing the url</returns>
-        public static string PayGramHyperLink(string botUsername, string? label = "PayGram", string? query = null)
+		/// <summary>
+		/// Makes a link to the PayGram bot specifing the shown label and eventually the parameters that follow the start
+		/// </summary>
+		/// <param name="label">What the user will see</param>
+		/// <param name="query">The parameters that follow the start. Should not include start=. If it is not a base64 string, it will be converted to base64</param>
+		/// <returns>The text containing the url</returns>
+		public static string PayGramHyperLink(string botUsername, string? label = "PayGram", string? query = null)
 		{
 			if (label == null) label = "";
 			if (query == null) query = "";
@@ -88,13 +89,16 @@ namespace PayGram.Public.Client
 		/// <param name="label">The text to display on the url</param>
 		/// <param name="callbackData">The data that you will receive when the users has completed the payment</param>
 		/// <returns>A payment url or null  if the value is &lt;=0</returns>
-		public static string? MakePaymentUrl(double amount, long toTid, string label, string callbackData)
+		public static string? MakePaymentUrl(string botUsername, long toTid, decimal amount, Currencies curr, string label, string callbackData)
 		{
 			if (amount <= 0) return null;
 
-			var query = $"{TAG_ACTION}={ACTION_PAY}&{TAG_TO}={toTid:X}&{TAG_AMOUNT}={amount.ToString("0.00", CultureInfo.InvariantCulture)}&{TAG_CALLBACKDATA}={callbackData}";
+			if (curr == Currencies.USD) curr = Currencies.USDT;
+			else if (curr == Currencies.PHP) curr = Currencies.PHPT;
 
-			return PayGramHyperLink(label, query);
+			var query = $"{TAG_ACTION}={ACTION_PAY}&{TAG_TO}={toTid:X}&{TAG_AMOUNT}={amount.ToString("0.00########", CultureInfo.InvariantCulture)}&{TAG_CURRENCY}={(int)curr}&{TAG_CALLBACKDATA}={callbackData}";
+
+			return PayGramHyperLink(botUsername, label, query);
 		}
 
 		/// <summary>
@@ -104,12 +108,15 @@ namespace PayGram.Public.Client
 		/// <param name="toTid">The telegram id of the user that will receive the payment</param>
 		/// <param name="callbackData">The data that you will receive when the users has completed the payment</param>
 		/// <returns>A payment url or null  if the value is &lt;=0</returns>
-		public static string? MakePaymentUrl(double amount, long toTid, string callbackData)
+		public static string? MakePaymentUrl(string botUsername, long toTid, decimal amount, Currencies curr, string callbackData)
 		{
 			if (amount <= 0) return null;
 
-			var query = $"{TAG_ACTION}={ACTION_PAY}&{TAG_TO}={toTid:X}&{TAG_AMOUNT}={amount.ToString("0.00", CultureInfo.InvariantCulture)}&{TAG_CALLBACKDATA}={callbackData}";
-			return PayGramOnlyLink(query);
+			if (curr == Currencies.USD) curr = Currencies.USDT;
+			else if (curr == Currencies.PHP) curr = Currencies.PHPT;
+
+			var query = $"{TAG_ACTION}={ACTION_PAY}&{TAG_TO}={toTid:X}&{TAG_AMOUNT}={amount.ToString("0.00######", CultureInfo.InvariantCulture)}&{TAG_CURRENCY}={(int)curr}&{TAG_CALLBACKDATA}={callbackData}";
+			return PayGramOnlyLink(botUsername, query);
 		}
 		/// <summary>
 		/// Creates a link to redeem an invoice
@@ -120,18 +127,18 @@ namespace PayGram.Public.Client
 		{
 			var cmd = TelegramCommand.MakeBase64($"{TAG_ACTION}{TelegramCommand.PARAMS_EQUAL_SEP}{ACTION_MAKEVOUCHER}{TelegramCommand.PARAMS_AND_SEP}{TAG_CODE}{TelegramCommand.PARAMS_EQUAL_SEP}{g}");
 			return $"{TELEGRAM_ADDRESS}{botName}?{TelegramCommand.START}{TelegramCommand.PARAMS_EQUAL_SEP}{cmd}";
-        }
-        public static string CancelWithdrawLink(Guid g, string botName)
-        {
-            var cmd = TelegramCommand.MakeBase64($"{TAG_ACTION}{TelegramCommand.PARAMS_EQUAL_SEP}{ACTION_CANCELWITHDRAW}{TelegramCommand.PARAMS_AND_SEP}{TAG_CODE}{TelegramCommand.PARAMS_EQUAL_SEP}{g}");
-            return $"{TELEGRAM_ADDRESS}{botName}?{TelegramCommand.START}{TelegramCommand.PARAMS_EQUAL_SEP}{cmd}";
-        }
-        /// <summary>
-        /// Creates a link to pay an invoice
-        /// </summary>
-        /// <param name="g"></param>
-        /// <returns></returns>
-        public static string MakeInvoiceLink(Guid g, string botName)
+		}
+		public static string CancelWithdrawLink(Guid g, string botName)
+		{
+			var cmd = TelegramCommand.MakeBase64($"{TAG_ACTION}{TelegramCommand.PARAMS_EQUAL_SEP}{ACTION_CANCELWITHDRAW}{TelegramCommand.PARAMS_AND_SEP}{TAG_CODE}{TelegramCommand.PARAMS_EQUAL_SEP}{g}");
+			return $"{TELEGRAM_ADDRESS}{botName}?{TelegramCommand.START}{TelegramCommand.PARAMS_EQUAL_SEP}{cmd}";
+		}
+		/// <summary>
+		/// Creates a link to pay an invoice
+		/// </summary>
+		/// <param name="g"></param>
+		/// <returns></returns>
+		public static string MakeInvoiceLink(Guid g, string botName)
 		{
 			var cmd = TelegramCommand.MakeBase64($"{TAG_ACTION}{TelegramCommand.PARAMS_EQUAL_SEP}{ACTION_MAKEINVOICE}{TelegramCommand.PARAMS_AND_SEP}{TAG_CODE}{TelegramCommand.PARAMS_EQUAL_SEP}{g}");
 			return $"{TELEGRAM_ADDRESS}{botName}?{TelegramCommand.START}{TelegramCommand.PARAMS_EQUAL_SEP}{cmd}";
