b5ac99799befded2eeced4f7d8b7b9b95c703769
Luigi Menghini <menghini@gmail.com>
2023-12-22 14:42:56 +0400

    sometimes a redenvelope showed as redeemable even though it was no longer redeemable

--- UserAPI/WithdrawAdminResponse.cs
+++ UserAPI/WithdrawAdminResponse.cs
@@ -7,11 +7,11 @@ namespace PayGram.Public.UserAPI
         /// <summary>
         /// Can be set by an admin, can be read by admins
         /// </summary>
-        public string AdminComment { get; set; }
+        public string? AdminComment { get; set; }
         /// <summary>
         /// Can be set by admins, can be read by authorized user/client
         /// </summary>
-        public string AdminResponse { get; set; }
+        public string? AdminResponse { get; set; }
         /// <summary>
         /// The status of the WithdrawInvoice. Setting this to InvoiceStatuses.Cancelled will not refund the user. 
         /// To Cancel and refund the user, set CancelWithdraw to true on RequestUpdateWithdraw
