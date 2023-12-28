a586a86160ba75ff95f16bf0b295e28a2c776cf1
Luigi Menghini <menghini@gmail.com>
2023-12-29 03:11:03 +0400

    Fixes RedEnvelope.IsRedeemed to consider amount and number of redeemers. other minor fixes

--- Responses/ResponseRedEnvelopeInvoiceInfo.cs
+++ Responses/ResponseRedEnvelopeInvoiceInfo.cs
@@ -1,10 +1,20 @@
 ﻿namespace PayGram.Public.Responses
 {
-	public record RedEnvRedeemer(decimal Amount, decimal Fee, string UserIdInClient);
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="Amount">The amount including the fees</param>
+	/// <param name="Fees">The fees</param>
+	/// <param name="UserIdInClient"></param>
+	/// <param name="RedeemedAtUtc"></param>
+	public record RedEnvRedeemer(decimal Amount, decimal Fees, string UserIdInClient, DateTime RedeemedAtUtc);
 
 	public class ResponseRedEnvelopeInvoiceInfo : ResponseInvoiceInfo
 	{
 		public decimal AvailableAmount => Amount - ExpectedFee - RedeemedAmount;
+		/// <summary>
+		/// The redeemed amount including the fees
+		/// </summary>
 		public decimal RedeemedAmount { get; set; }
 		public int RedeemersCount { get; set; }
 		public int MaxRedeemers { get; set; }
