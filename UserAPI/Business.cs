b296bf89ca003648d6d390da246b97fd8ab57666
Luigi Menghini <menghini@gmail.com>
2023-12-31 13:35:11 +0400

    handles usernames that contain html tags as well other text that might contain html tags

--- UserAPI/Business.cs
+++ UserAPI/Business.cs
@@ -1,13 +1,20 @@
-﻿namespace PayGram.Public.UserAPI
+﻿using Newtonsoft.Json;
+using System.Web;
+
+namespace PayGram.Public.UserAPI
 {
 	public class Business
 	{
 		public long Id { get; set; }
 		public string Name { get; set; }
+		[JsonIgnore]
+		public string? NameHtmlEncoded => HttpUtility.HtmlEncode(Name);
 		public string OwnerIdInClient { get; set; }
 		public bool IsOnline { get; set; }
 		public string? LocationUrl { get; set; }
 		public string? ImageUrl { get; set; }
+		[JsonIgnore]
+		public string? DescriptionHtmlEncoded => HttpUtility.HtmlEncode(Description);
 		public string? Description { get; set; }
 		public bool IsApproved { get; set; }
         public bool Deleted { get; set; }
--- UserAPI/P2PGroup.cs
+++ UserAPI/P2PGroup.cs
@@ -1,4 +1,6 @@
 ﻿using CurrenciesLib;
+using Newtonsoft.Json;
+using System.Web;
 
 namespace PayGram.Public.UserAPI
 {
@@ -6,11 +8,15 @@ namespace PayGram.Public.UserAPI
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
+		[JsonIgnore]
+		public string? NameHtmlEncoded => HttpUtility.HtmlEncode(Name);
 		public string? Link { get; set; }
 		public P2PGroupType P2PGroupType { get; set; } = P2PGroupType.None;
 		public bool Status { get; set; }
 		public string? Notes { get; set; }
-		public decimal FixedFee { get; set; }
+		[JsonIgnore]
+		public string? NotesHtmlEncoded { get; set; }
+        public decimal FixedFee { get; set; }
 		public double PercFee { get; set; }
 		public double ChangeRate { get; set; }
 		public decimal MaxBuy { get; set; }
